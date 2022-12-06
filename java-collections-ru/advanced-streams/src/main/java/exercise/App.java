8b99bd07d7242ecf6a719cefee5a9a5a95ba4cda
@hexlet/cli <support@hexlet.io>
2022-12-06 19:40:17 +0300

    submit java-collections-ru/advanced-streams

---
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+// BEGIN
+/*class App {
+    public static String getForwardedVariables(String confFile) {
+        List<String> resultList = Arrays.stream(confFile.split("\n"))
+                .filter(x -> x.startsWith("environment"))
+                .map(x -> x.replaceAll("environment=", ""))
+                .flatMap(x -> Arrays.stream(x.split(",")))
+                .filter(x -> x.startsWith("X_FORWARDED_"))
+                .map(x -> x.replaceAll("X_FORWARDED_", ""));
+        String result = "";
+        for (String item : resultList) {
+            result += item + ", ";
+        }
+        return result;
+    }
+}*/
+class App {
+    public static String getForwardedVariables(String confFile) {
+        String[] fixparam = Arrays.stream(confFile.split("\n"))
+                .filter(x -> x.startsWith("environment"))
+                .map(x -> x.replaceAll("environment=", ""))
+                .flatMap(x -> Arrays.stream(x.split(",")))
+                .filter(x -> x.startsWith("X_FORWARDED_"))
+                .map(x -> x.replaceAll("X_FORWARDED_", ""))
+                .toArray(String[]::new);
+        String result = "";
+        for (String i : fixparam ) {
+            result += i;
+        }
+        return result;
+    }
+}
+//END

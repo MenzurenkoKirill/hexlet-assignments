9d40fd28d3078b3bd291aa9293b1a5c0cfad1cf3
@hexlet/cli <support@hexlet.io>
2023-03-05 20:37:24 +0300

    submit java-web-ru/html

--- java-web-ru/html/src/main/java/exercise/servlet/UsersServlet.java
+++ java-web-ru/html/src/main/java/exercise/servlet/UsersServlet.java
@@ -105,10 +105,7 @@ public class UsersServlet extends HttpServlet {
                  throws IOException {
 
         // BEGIN
-        List<Map<String, String>> user = getUsers().stream()
-                .filter(x -> x.get("id").equals(id))
-                .findFirst()
-                .orElse(null);
+        Map<String, String> user = getUser(getUsers(), id);
         if (user == null) {
             response.sendError(404, "Not found");
         } else {
@@ -116,43 +113,50 @@ public class UsersServlet extends HttpServlet {
             response.setContentType("text/html;charset=UTF-8");
             StringBuilder body = new StringBuilder();
             body.append("""
-                <!DOCTYPE html>
-                    <html lang="ru">
-                        <head>
-                            <meta charset="UTF-8">
-                            <title>Example application</title>
-                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-                                  rel="stylesheet"
-                                  integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-                                  crossorigin="anonymous">
-                        </head>
-                        <style>
-                        table, th, td {
-                          border: 3px solid blue;
-                        }
-                        </style>
-                        <body>
-                        <div class="container">
-                            <table style="width:50%">
-                                <tr>
-                                    <th>id</th>
-                                    <th>full name</th>
-                                    <th>email</th>
-                                </tr>
-                    """);
-            body.append( "<tr>"
+                    <!DOCTYPE html>
+                        <html lang="ru">
+                            <head>
+                                <meta charset="UTF-8">
+                                <title>Example application</title>
+                                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+                                      rel="stylesheet"
+                                      integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+                                      crossorigin="anonymous">
+                            </head>
+                            <style>
+                            table, th, td {
+                              border: 3px solid blue;
+                            }
+                            </style>
+                            <body>
+                            <div class="container">
+                                <table style="width:50%">
+                                    <tr>
+                                    
+                                        <th>id</th>
+                                        <th>full name</th>
+                                        <th>email</th>
+                                    </tr>
+                        """);
+            body.append("<tr>"
                     + "<td>" + user.get("id") + "</td>"
                     + "<td>" + user.get("firstName") + " " + user.get("lastName") + "</td>"
                     + "<td>" + user.get("email") + "</td>" + "</a></td>"
                     + "</tr>");
             body.append("""
-                                        </table>
-                                    </div>
-                                </body>
-                            </html>
-                            """);
+                                </table>
+                            </div>
+                        </body>
+                    </html>
+                    """);
             out.println(body);
         }
+    }
+    private Map<String, String> getUser(List<Map<String, String>> users, String id) throws JsonProcessingException, IOException {
+        return users.stream()
+                    .filter(u -> u.get("id").equals(id))
+                    .findFirst()
+                    .orElse(null);
             // END
     }
 }
